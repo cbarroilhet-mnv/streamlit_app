1c823a9a449c95a6bccd4d08b72d72135db77718
Camilo Barroilhet <82624397+cbarroilhet-mnv@users.noreply.github.com>
2023-10-23 17:37:54 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,23 +2,20 @@ from snowflake.snowpark.session import Session
 from snowflake.snowpark.functions import avg, sum, col, lit
 import streamlit as st
 import pandas as pd
+import snowflake.connector
 
 # Create Session object
-def create_session_object():
-   connection_parameters = {
-      "account": "ABYLBQO-ZJ05545",
-      "user": "CAMILOB",
-      "password": "D@t@verse$$1",
-      "role": "ACCOUNTADMIN",
-      "warehouse": "LEASE_WH",
-      "database": "STREAMLIT_APPS",
-      "schema": "WES_POC"
-   }
-   session = Session.builder.configs(connection_parameters).create()
-   user = session.sql('select current_user()').collect()
+conn = snowflake.connector.connect(
+    user='CAMILOB',
+    password='D@t@verse$$1',
+    account='ABYLBQO-ZJ05545',
+    warehouse='LEASE_WH',
+    database='STREAMLIT_APPS',
+    schema='WES_POC'
+)
 
-   # Use Streamlit to display the current user
-   st.write("Current User:", user[0][0])
-
-    # Call the function to create the Snowflake session and display the current user
-create_session_object()
+# Create a cursor
+cursor = conn.cursor()
+cursor.execute("select current_user()")
+current_user = cursor.fetch_pandas_all().iloc[0]
+st.info(current_user)
