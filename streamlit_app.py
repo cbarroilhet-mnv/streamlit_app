75a423865296dfd4a6ff00566faed73c5e5f3ad7
Camilo Barroilhet <82624397+cbarroilhet-mnv@users.noreply.github.com>
2023-10-23 17:43:08 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,23 @@
 from snowflake.snowpark import Session
+import streamlit as st
 
 # Create Session object
 connection_parameters = {
-    user='CAMILOB',
-    password='D@t@verse$$1',
-    account='ABYLBQO-ZJ05545',
-    warehouse='LEASE_WH',
-    database='STREAMLIT_APPS',
-    schema='WES_POC'
+    'user': 'CAMILOB',
+    'password': 'D@t@verse$$1',
+    'account': 'ABYLBQO-ZJ05545',
+    'warehouse': 'LEASE_WH',
+    'database': 'STREAMLIT_APPS',
+    'schema': 'WES_POC'
 }
 
 test_session = Session.builder.configs(connection_parameters).create()
 
-ev_sales = test_session.sql(("select current_user()").collect()
+# Execute the SQL query
+ev_sales = test_session.execute("SELECT current_user()")
+
+# Convert the result to a Pandas DataFrame
 ev_sales_df = ev_sales.to_pandas()
+
+# Display the DataFrame using Streamlit
 st.dataframe(ev_sales_df)
